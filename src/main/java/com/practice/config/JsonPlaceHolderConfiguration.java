294c5f293ce3263cfcc40795be8ae0b012ff9867
Lawal A. Hamisu <lahamisuahmed@gmail.com>
2022-02-03 12:58:54 +0100

    add custom error handling for openfeign client

--- src/main/java/com/practice/config/JsonPlaceHolderConfiguration.java
+++ src/main/java/com/practice/config/JsonPlaceHolderConfiguration.java
@@ -1,5 +1,7 @@
 package com.practice.config;
 
+import com.practice.exception.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
 import feign.okhttp.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 
@@ -8,5 +10,10 @@ public class JsonPlaceHolderConfiguration {
     @Bean
     public OkHttpClient client(){
         return new OkHttpClient();
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
     }
 }
